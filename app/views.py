515ebdc9cea8d31bf79ebc4cd543f9f12105775f
Marionjames-ally <marionjames2018@gmail.com>
2020-01-29 22:48:59 +0300

    add templates and a view function

--- app/views.py
+++ app/views.py
@@ -8,4 +8,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
