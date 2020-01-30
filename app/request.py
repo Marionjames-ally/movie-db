bce28a03c89c85062ae800f4ce68469c75ccc5a7
Marionjames-ally <marionjames2018@gmail.com>
2020-01-30 16:39:09 +0300

    include a search function using the api

--- app/request.py
+++ app/request.py
@@ -11,6 +11,25 @@ api_key = app.config['MOVIE_API_KEY']
 base_url = app.config["MOVIE_API_BASE_URL"]
 
 
+def get_movies(category):
+    """
+    Function that gets the json response to our url request
+    """
+    get_movies_url = base_url.format(category, api_key)
+
+    with urllib.request.urlopen(get_movies_url) as url:
+        get_movies_data = url.read()
+        get_movies_response = json.loads(get_movies_data)
+
+        movie_results = None
+
+        if get_movies_response['results']:
+            movie_results_list = get_movies_response['results']
+            movie_results = process_results(movie_results_list)
+
+    return movie_results
+
+
 def get_movie(id):
     get_movie_details_url = base_url.format(id, api_key)
 
@@ -56,3 +75,18 @@ def process_results(movie_list):
             movie_results.append(movie_object)
 
     return movie_results
+
+
+def search_movie(movie_name):
+    search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key, movie_name)
+    with urllib.request.urlopen(search_movie_url) as url:
+        search_movie_data = url.read()
+        search_movie_response = json.loads(search_movie_data)
+
+        search_movie_results = None
+
+        if search_movie_response['results']:
+            search_movie_list = search_movie_response['results']
+            search_movie_results = process_results(search_movie_list)
+
+    return search_movie_results
