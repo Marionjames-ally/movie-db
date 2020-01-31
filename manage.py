514d831e50ed554dad1d7fd55068f96ae88c3efe
Marionjames-ally <marionjames2018@gmail.com>
2020-01-31 13:55:20 +0300

    deployment

--- manage.py
+++ manage.py
@@ -1,17 +1,16 @@
 from app import create_app
-from flask_script import Manager, Shell, Server
+from flask_script import Manager, Server
 
 app = create_app('development')
 manager = Manager(app)
 manager.add_command('server', Server)
 
+manager.add_command('server', Server)
 
-@manager.add_command
+
+@manager.command
 def test():
-    """
-    run the unit test
-    :return:
-    """
+    """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
